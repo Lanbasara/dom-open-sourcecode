82e66f67ae0449db9c352b4d8a4fa8c645605248
Lanbasara <ghkagg@outlook.com>
2023-01-16 13:02:17 +0800

    0.17.0-0

--- scripts/git-tag.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('shelljs')
-const npm_package_version = env['npm_package_version'];
-
-console.log('tag version is',`v${npm_package_version}`)
-
-exec(`git tag v${npm_package_version}`)
-
-exec(`git push origin v${npm_package_version}`)
