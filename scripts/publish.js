6663917a6872094680928dfd530c5b1c2680ad73
Lanbasara <ghkagg@outlook.com>
2023-01-16 16:58:46 +0800

    update publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,10 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
-const { exec } = require("shelljs");
+const shell = require("shelljs");
+const { exec, env } = shell
+
+const prevVersion = env['npm_package_version'];
+
 
 const PUBLISH_TYPE = [
   { name: "preminor", value: "preminor" },
@@ -18,38 +22,62 @@ const questions = [
     default: "patch",
     choices: PUBLISH_TYPE,
   },
-  {
-    type: "confirm",
-    name: "git_tag",
-    message: "Create a git tag?",
-    default: false,
-  },
 ];
 
 inquirer.prompt(questions).then((ans) => {
   const type = ans["publish_type"];
 
-  const isGitTag = ans["git_tag"];
+  console.log(console.log(chalk.blue(`1. Start publish new ${type} version`)));
 
-  console.log(console.log(chalk.blue("1. start publish new version")));
-
-  console.log(chalk.blue("2. compiling...."));
+  console.log(chalk.blue("2. Start compiling...."));
 
   exec("npm run build");
 
-  exec(`npm version ${type}`);
 
-  console.log(chalk.blue("3. publishing..."));
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "update_version",
+      message: `Current version is ${prevVersion}, do ${type} update and publish?`,
+      default: false,
+    })
+    .then((ans) => {
+      const isUpdate = ans["update_version"];
+      if (isUpdate) {
+        console.log(chalk.blue("3. Start update version"));
 
-  exec("npm publish");
+        if (exec(`npm --no-git-tag-version version ${type}`).code !== 0) {
+          shell.echo("Error: npm version error");
+          shell.exit(1);
+        }
 
-  if (isGitTag) {
-    const version = require("../package.json").version;
+        console.log(chalk.blue("4. Publishing..."));
 
-    console.log(chalk.blue("Start create git tag, version is", `v${version}`));
+        exec("npm publish");
 
-    exec(`git push origin v${version}`);
+        inquirer
+          .prompt({
+            type: "confirm",
+            name: "git_tag",
+            message: "Create a git tag?",
+            default: false,
+          })
+          .then((ans) => {
+            const isGitTag = ans["git_tag"];
+            if (isGitTag) {
+              const version = require("../package.json").version;
 
-    console.log(chalk.blue("Finish git tag create and publish"));
-  }
+              console.log(
+                chalk.blue("Start create git tag, version is", `v${version}`)
+              );
+
+              exec(`git tag v${version}`)
+
+              exec(`git push origin v${version}`);
+
+              console.log(chalk.blue("Finish git tag create and publish"));
+            }
+          });
+      }
+    });
 });
