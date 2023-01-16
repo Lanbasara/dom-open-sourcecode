d2b0a68f8aeb500f4c63cb3ff9ed570d0cbd48f4
Lanbasara <ghkagg@outlook.com>
2023-01-16 12:12:37 +0800

    0.10.0-0

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,19 +2,22 @@ const { exec, env } = require("shelljs");
 
 const versionTitle = process.argv.slice(2)[0].match(/--(\w+)/)[1];
 
+let npm_package_version
+
 console.log("1. start publish new version");
 
 console.log("2. compiling....");
 
 exec("npm run build");
 
-exec(`npm version ${versionTitle}`);
+exec(`npm version ${versionTitle}`,{async:true}, () => {
+    npm_package_version =  env["npm_package_version"];
+});
 
 console.log("3. publishing...");
 
 exec("npm publish");
 
-const npm_package_version = env["npm_package_version"];
 
 console.log(`version tag is ${npm_package_version}`);
 
